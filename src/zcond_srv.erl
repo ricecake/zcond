51fad3464343b2efea71f7adf378696782be341e
Sebastian Green-Husted <ricecake@tfm.nu>
2016-01-25 19:19:10 -0500

    respond to broadcast message

--- src/zcond_srv.erl
+++ src/zcond_srv.erl
@@ -43,6 +43,7 @@ init(Args) ->
 		{add_membership, {{239,0,0,239}, {0,0,0,0}}},
 		binary
 	]),
+	io:format("~p~n", [inet:socknames(Socket)]),
 	erlang:send_after(1000, self(), tick),
 	{ok, Args#{ socket => Socket }}.
 
@@ -54,11 +55,15 @@ handle_cast(_Msg, State) ->
 
 handle_info(tick, #{ socket := Socket } = State) ->
 	Name = erlang:atom_to_binary(node(), utf8),
-	gen_udp:send(Socket, {239,0,0,239}, 1970, <<"butts", 1:8, (size(Name)):8, Name/binary>>),
+	gen_udp:send(Socket, {239,0,0,239}, 1970, <<"butt", 1:8, 0:8, (size(Name)):8, Name/binary>>),
 	erlang:send_after(1000, self(), tick),
 	{noreply, State};
-handle_info({udp, _Socket, IP, Port, Packet}, State) ->
-	io:format("GOT ~p~n", [{IP, Port, Packet}]),
+handle_info({udp, _Socket, IP, 1970, <<"butt", 1:8, 0:8, Size:8, Name:Size/binary>>}, #{ socket := Socket } = State) ->
+	io:format("GOT ~p~n", [{IP, Name}]),
+	gen_udp:send(Socket, IP, 1970, <<"butt", 1:8, 1:8, "HELLO">>),
+	{noreply, State};
+handle_info({udp, _Socket, _IP, 1970, <<"butt", 1:8, 1:8, Rest/binary>>}, State) ->
+	io:format("GOT ~s~n", [Rest]),
 	{noreply, State};
 handle_info(_Info, State) ->
 	{noreply, State}.
