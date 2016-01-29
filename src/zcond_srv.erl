918875b54891b27932eb5e9f9ad683362a52da0c
Sebastian Green-Husted <ricecake@tfm.nu>
2016-01-28 19:00:56 -0500

    startup clustering when broadcast hits

--- src/zcond_srv.erl
+++ src/zcond_srv.erl
@@ -44,10 +44,9 @@ init(Args) ->
 		binary
 	]),
 	Name = erlang:atom_to_binary(node(), utf8),
-	Announce  = <<"butt", 1:8, 0:8, (size(Name)):8, Name/binary>>,
-	Introduce = <<"butt", 1:8, 1:8, (size(Name)):8, Name/binary>>,
+	Announce  = <<"butt", 0:8, (size(Name)):8, Name/binary>>,
 	erlang:send_after(1000, self(), tick),
-	{ok, Args#{ socket => Socket, announce => Announce, intro => Introduce }}.
+	{ok, Args#{ socket => Socket, announce => Announce }}.
 
 handle_call(_Msg, _From, State) ->
 	{reply, ok, State}.
@@ -59,16 +58,15 @@ handle_info(tick, #{ socket := Socket, announce := Announce } = State) ->
 	gen_udp:send(Socket, {239,0,0,239}, 1970, Announce),
 	erlang:send_after(1000, self(), tick),
 	{noreply, State};
-handle_info({udp, _Socket, _IP, 1970, Introduce}, #{ intro := Introduce } = State) ->
-	{noreply, State};
 handle_info({udp, _Socket, _IP, 1970, Announce}, #{ announce := Announce } = State) ->
 	{noreply, State};
-handle_info({udp, _Socket, IP, 1970, <<"butt", 1:8, 0:8, Size:8, Name:Size/binary>>}, #{ socket := Socket, intro := Intro } = State) ->
-	io:format("GOT anounce ~p~n", [{IP, Name}]),
-	gen_udp:send(Socket, IP, 1970, Intro),
-	{noreply, State};
-handle_info({udp, _Socket, IP, 1970, <<"butt", 1:8, 1:8, Size:8, Name:Size/binary>>}, State) ->
-	io:format("GOT intro ~p~n", [{IP, Name}]),
+handle_info({udp, _Socket, _IP, 1970, <<"butt", 0:8, Size:8, Name:Size/binary>>}, State) ->
+	Node = erlang:binary_to_atom(Name, utf8),
+	AlreadyConnected = lists:member(Node, nodes(connected)),
+	if
+		AlreadyConnected -> ok;
+		not AlreadyConnected -> net_adm:ping(Node)
+	end,
 	{noreply, State};
 handle_info(_Info, State) ->
 	{noreply, State}.
